8262afba57f32ada74fed9fd394d543fc05ed684
Perrine Deserville <mondierep@gmail.com>
2024-08-05 09:24:56 +0200

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,43 @@
+
+import ChickenCard from './components/ChickenCard';
+import chickenData from "../chickenTab.json";
+
 import './App.css'
 
 function App() {
+  const teachers = chickenData.chickenTab.filter((teacher) => teacher.position === "Teacher");
+
+  const students = chickenData.chickenTab.filter((student) => student.position === "Student");
+
 
   return (
-    <>
-    </>
+    <div>
+      <section className="teachers">
+        <h2>Équipe pédagogique</h2>
+        {teachers.map((teacher) => 
+          <ChickenCard
+            key={teacher.id}
+            imgSrc={teacher.imgSrc}
+            name={teacher.name}
+            role={teacher.position}
+            description={teacher.biography}
+          />
+        )}
+      </section>
+
+      <section>
+        <h2>Camarades</h2>
+          {students.map((student) => 
+          <ChickenCard
+            key={student.id}
+            imgSrc={student.imgSrc}
+            name={student.name}
+            role={student.position}
+            description={student.biography}
+          />
+        )}
+      </section>
+    </div>
   )
 }
 
--- src/components/ChickenCard.jsx
+++ src/components/ChickenCard.jsx
@@ -1,7 +1,11 @@
-function ChickenCard () {
+function ChickenCard ({imgSrc, name, role, description}) {
     return (
-        <>
-        </>
+        <div className="chicken-card">
+            <img src={imgSrc} alt={name} />
+            <h2>{name}</h2>
+            <h3>{role}</h3>
+            <p>{description}</p>
+        </div>
     )
 }
 
